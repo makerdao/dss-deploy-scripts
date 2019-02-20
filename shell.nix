e7822918e83705e0973a8e365a7615e1b38a4c2d
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-02-21 00:53:27 +0100

    nix: make dapptools configurable

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
 { nixpkgs ? <nixpkgs>
+, dapptools ? fetchGit {
+    url = https://github.com/dapphub/dapptools.git;
+    ref = dapptoolsRev;
+  }
+, dapptoolsRev ? "ce05606a8e8f76bced691d6e03c8625862c17d97"
 , eth_from
 , eth_keystore ? ~/.dapp/testnet/8545/keystore
 , eth_password ? "/dev/null"
 }:
 
 let
-  dapptools = import ((fetchGit {
-    url = https://github.com/dapphub/dapptools.git;
-    ref = "ce05606a8e8f76bced691d6e03c8625862c17d97";
-  }) + "/overlay.nix");
-  pkgs = import nixpkgs { overlays = [ dapptools ]; };
+  pkgs = import nixpkgs { overlays = [ (import "${dapptools}/overlay.nix") ]; };
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [ bc dapp ethsign seth ];
