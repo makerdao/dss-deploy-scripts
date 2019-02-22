a19bcd340db160fd19d624d7b5096daa9dd194db
Christopher Fredén <christopherf@makerdao.com>
2019-02-22 12:09:28 +0100

    Use dapptools own version of nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,21 +1,18 @@
-{ nixpkgs ? <nixpkgs>
-, dapptools ? fetchGit {
+{ pkgs ? import <nixpkgs> {}
+, dapptools ? import (fetchGit {
     url = https://github.com/dapphub/dapptools.git;
     ref = dapptoolsRev;
-  }
+  }) {}
 , dapptoolsRev ? "ce05606a8e8f76bced691d6e03c8625862c17d97"
 , eth_from
 , eth_keystore ? ~/.dapp/testnet/8545/keystore
 , eth_password ? "/dev/null"
 }:
 
-let
-  pkgs = import nixpkgs { overlays = [ (import "${dapptools}/overlay.nix") ]; };
-in
-  pkgs.mkShell {
-    buildInputs = with pkgs; [ bc dapp ethsign seth ];
+pkgs.mkShell {
+  buildInputs = with pkgs; with dapptools; [ bc dapp ethsign seth ];
 
-    ETH_FROM="${eth_from}";
-    ETH_KEYSTORE="${eth_keystore}";
-    ETH_PASSWORD="${eth_password}";
-  }
+  ETH_FROM="${eth_from}";
+  ETH_KEYSTORE="${eth_keystore}";
+  ETH_PASSWORD="${eth_password}";
+}
