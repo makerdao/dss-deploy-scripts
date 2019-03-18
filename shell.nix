66ef24fd0180c7470025bb858bff85a0820d332a
Christopher Fredén <christopherf@makerdao.com>
2019-03-18 10:04:29 +0100

    Pin mcd CLI version explicitly

--- shell.nix
+++ shell.nix
@@ -1,15 +1,23 @@
 { pkgs ? import (fetchGit {
     url = https://github.com/dapphub/dapptools.git;
-    ref = dapptoolsRev;
+    rev = dapptools-rev;
   }) {}
-, dapptoolsRev ? "6943c76bfb8e0b1fce54c3d9bba6f0f7e50d2f5c"
+, dapptools-rev ? "6943c76bfb8e0b1fce54c3d9bba6f0f7e50d2f5c"
+, mcd-cli ? pkgs.callPackage (import (fetchGit {
+    url = https://github.com/makerdao/mcd-cli.git;
+    rev = mcd-cli-rev;
+  })) {}
+, mcd-cli-rev ? "86842b49defa53301ac0019f7d5994859bb3e1e9"
 , eth_from
 , eth_keystore ? ~/.dapp/testnet/8545/keystore
 , eth_password ? "/dev/null"
 }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ bc dapp jq ethsign seth ];
+  buildInputs = with pkgs; [
+    dapp ethsign seth mcd-cli
+    bc jq coreutils findutils procps
+  ];
 
   ETH_FROM="${eth_from}";
   ETH_KEYSTORE="${eth_keystore}";
