ac60b2ca129fa181ec55acc9e854469d895464b5
Christopher Fredén <christopherf@makerdao.com>
2019-03-22 10:47:21 +0100

    Removed dependency revision arguments from shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,11 @@
 { pkgs ? import (fetchGit {
     url = https://github.com/dapphub/dapptools.git;
-    rev = dapptools-rev;
+    rev = "6943c76bfb8e0b1fce54c3d9bba6f0f7e50d2f5c";
   }) {}
-, dapptools-rev ? "6943c76bfb8e0b1fce54c3d9bba6f0f7e50d2f5c"
 , mcd-cli ? pkgs.callPackage (import (fetchGit {
     url = https://github.com/makerdao/mcd-cli.git;
-    rev = mcd-cli-rev;
+    rev = "86842b49defa53301ac0019f7d5994859bb3e1e9";
   })) {}
-, mcd-cli-rev ? "86842b49defa53301ac0019f7d5994859bb3e1e9"
 , eth_from
 , eth_keystore ? ~/.dapp/testnet/8545/keystore
 , eth_password ? "/dev/null"
@@ -16,7 +14,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     dapp ethsign seth mcd-cli
-    bc jq coreutils findutils procps
+    bc jq coreutils
   ];
 
   ETH_FROM="${eth_from}";
