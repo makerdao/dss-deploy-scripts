dc827129664d4e27a1722b6f2d1a99f32dbbbf2b
Christopher Fredén <christopherf@makerdao.com>
2019-05-16 19:28:59 +0200

    Move maker specific package to nixpkgs-pin

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,27 +1,5 @@
-let
-  nixpkgsPin = import (fetchGit {
-    url = "https://github.com/makerdao/nixpkgs-pin";
-    ref = "master";
-  });
-  #import (../../nixpkgs-pin)
-in
-
-args:
-
-let
-  pkgsPin = nixpkgsPin args;
-  pkgs = pkgsPin.pkgs;
-in pkgsPin // {
-    pkgs = pkgs // {
-      # Add mcd-cli and sethret to local scope
-      mcd-cli = pkgs.callPackage (import (fetchGit {
-        url = https://github.com/makerdao/mcd-cli.git;
-        rev = "86842b49defa53301ac0019f7d5994859bb3e1e9";
-      })) {};
-
-      sethret = (import (fetchGit {
-        url = https://github.com/icetan/sethret.git;
-        rev = "ef77915e2881011603491275f36b44bf2478b408";
-      }) { inherit pkgs; }).sethret;
-    };
-  }
+import (fetchGit {
+  url = "https://github.com/makerdao/nixpkgs-pin";
+  rev = "16427c46f6472c040a600a527dd9da6e505c8b8e";
+  ref = "master";
+})
