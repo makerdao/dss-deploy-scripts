f0bcb9eedd8ad89927e6b6c016581139061ae2b6
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-06-05 16:58:01 -0400

    clear up other travis quote warnings

--- lib/common.sh
+++ lib/common.sh
@@ -84,7 +84,7 @@ jq_inplace() {
   local tmp; tmp=$(mktemp)
   local query; query=$1
   local file; file=$2
-  jq "$query" "$file" > $tmp && mv $tmp "$file"
+  jq "$query" "$file" > "$tmp" && mv "$tmp" "$file"
 }
 
 # Start verbose output
