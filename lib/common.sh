994a719c24ba6929f7a57603ca1674dde3ed2659
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-06-05 16:11:24 -0400

    create tokens and osm before running kovan-deploy script

--- lib/common.sh
+++ lib/common.sh
@@ -80,6 +80,13 @@ err() {
     printf '%b\n' "${RED}❌${1}${NC}"
 }
 
+jq_inplace() {
+  local tmp; tmp=$(mktemp)
+  local query; query=$1
+  local file; file=$2
+  jq "$query" "$file" > $tmp && mv $tmp "$file"
+}
+
 # Start verbose output
 set -x
 
