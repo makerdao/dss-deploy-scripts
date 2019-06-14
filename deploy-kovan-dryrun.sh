eb5ed83d3ff44d571327ca9f0cef0b25585e39e0
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-06-14 12:06:21 +0200

    update kovan dryrun to abstract deploying pips

---
+++ deploy-kovan-dryrun.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# shellcheck source=lib/common.sh
+. "${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
+setConfigFile "kovan-dryrun"
+
+# Setup steps
+
+# Send ETH to Omnia Relayer
+OMNIA_RELAYER=$(jq -r ".omniaFromAddr" "$CONFIG_FILE")
+seth send "$OMNIA_RELAYER" --value "$(seth --to-wei 10000 eth)"
+
+# Export it so we can use it in pips-deploy
+export OMNIA_RELAYER
+
+# shellcheck source=/dev/null
+"$LIBEXEC_DIR"/pips-deploy
+
+# Run Deployment
+"$BIN_DIR"/deploy-kovan.sh
+
+echo "KOVAN DRYRUN PREP COMPLETED SUCCESSFULLY"
