8a3d330a9ef7cbb5084a8a15f7cbba3b53f533be
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-06-17 21:54:36 +0200

    make setting the config step optional to allow for stacking steps

--- deploy-kovan.sh
+++ deploy-kovan.sh
@@ -6,14 +6,14 @@ writeConfigFor "kovan"
 
 if [[ "$CONFIG_STEP" == "kovan" ]]; then
     test "$(seth chain)" == "kovan" || exit 1
+    # Set verify contract option in Etherscan if the API key is in the config file
+    etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
+    if [[ "$etherscanApiKey" != "" ]]; then
+        export DAPP_VERIFY_CONTRACT="yes"
+        export ETHERSCAN_API_KEY=$etherscanApiKey
+    fi
 fi
 
-# Set verify contract option in Etherscan if the API key is in the config file
-etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
-if [[ "$etherscanApiKey" != "" ]]; then
-    export DAPP_VERIFY_CONTRACT="yes"
-    export ETHERSCAN_API_KEY=$etherscanApiKey
-fi
 
 "$LIBEXEC_DIR"/base-deploy
 
