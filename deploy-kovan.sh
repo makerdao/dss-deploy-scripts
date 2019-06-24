bb8566e3a3d1021b3a6319b1c5a9eb5b50aef0ce
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-06-24 14:10:21 +0200

    update deploy kovan like to setup requirements for a kovan like deployment locally

--- deploy-kovan.sh
+++ deploy-kovan.sh
@@ -4,16 +4,14 @@
 . "${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
 writeConfigFor "kovan"
 
-if [[ "$CONFIG_STEP" == "kovan" ]]; then
-    test "$(seth chain)" == "kovan" || exit 1
-    # Set verify contract option in Etherscan if the API key is in the config file
-    etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
-    if [[ "$etherscanApiKey" != "" ]]; then
-        export DAPP_VERIFY_CONTRACT="yes"
-        export ETHERSCAN_API_KEY=$etherscanApiKey
-    fi
-fi
+test "$(seth chain)" == "kovan" || exit 1
 
+# Set verify contract option in Etherscan if the API key is in the config file
+etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
+if [[ "$etherscanApiKey" != "" ]]; then
+    export DAPP_VERIFY_CONTRACT="yes"
+    export ETHERSCAN_API_KEY=$etherscanApiKey
+fi
 
 "$LIBEXEC_DIR"/base-deploy
 
--- scripts/setup/prep-kovan-like.sh
+++ scripts/setup/prep-kovan-like.sh
@@ -1,21 +1,79 @@
 #!/usr/bin/env bash
 
+jq_inplace() {
+  local tmp; tmp=$(mktemp)
+  local query; query=$1
+  local file; file=$2
+  jq "$query" "$file" > "$tmp" && mv "$tmp" "$file"
+}
+
 # shellcheck source=lib/common.sh
-. "${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
-setConfigFile "kovan-dryrun"
+. "./lib/common.sh"
+# . "../../${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
+export SETUP_CONFIG_FILE="${LIBEXEC_DIR:-$BIN_DIR}/setup/prep-kovan-like.json"
 
 # Setup steps
 
 # Send ETH to Omnia Relayer
-OMNIA_RELAYER=$(jq -r ".omniaFromAddr" "$CONFIG_FILE")
+OMNIA_RELAYER=$(jq -r ".omniaFromAddr" "$SETUP_CONFIG_FILE")
 seth send "$OMNIA_RELAYER" --value "$(seth --to-wei 10000 eth)"
 
-# Export it so we can use it in pips-deploy
-export OMNIA_RELAYER
+# Deploy Values or Medians + OSMs (if delay > 0) (no solc optimization)
+dappBuild osm
+dappBuild testchain-medians
 
-"$LIBEXEC_DIR"/pips-deploy
+tokens=$(jq -r ".tokens | keys_unsorted[]" "$SETUP_CONFIG_FILE")
+for token in $tokens; do
+    type=$(jq -r ".tokens.${token} | .pip | .type" "$SETUP_CONFIG_FILE")
+    # Deploy Medianizer as Feed
+    if [[ "$type" == "median" ]]; then
+        eval "export VAL_$token=$(dappCreate testchain-medians "Median${token}USD")"
+        signers=$(jq -r ".tokens.${token} | .pip | .signers | .[]" "$SETUP_CONFIG_FILE")
+        # Approve oracle price feed providers
+        for signer in $signers; do
+            seth send "$(eval echo "\$VAL_${token}")" 'lift(address)' "$signer"
+        done
+        # Set quorum for Medianizer
+        seth send "$(eval echo "\$VAL_${token}")" 'setBar(uint256)' "$(seth --to-uint256 3)"
+        # Whitelist Omnia relayer to read price from Medianizer
+        seth send "$(eval echo "\$VAL_${token}")" 'kiss(address)' "$OMNIA_RELAYER"
+    fi
+    # Deploy DSValue as Feed
+    if [[ "${type}" == "value" ]]; then
+        eval "export VAL_$token=$(dappCreate osm DSValue)"
+    fi
+    # Deploy OSM if delay > 0
+    osm_delay=$(jq -r ".tokens.${token} | .pip | .osmDelay" "$SETUP_CONFIG_FILE")
+    if [[ "$osm_delay" -gt 0 ]]; then
+        # Deploy OSM
+        eval "export PIP_$token=$(dappCreate osm OSM "$(eval echo "\$VAL_${token}")")"
+        # Set OSM delay
+        seth send "$(eval echo "\$PIP_${token}")" 'step(uint16)' "$osm_delay"
+        # Whitelist OSM in Medianizer (skip if source is DSValue)
+        [[ "$type" == "median" ]] && seth send "$(eval echo "\$VAL_${token}")" 'kiss(address)' "$(eval echo "\$PIP_${token}")"
+    else
+        eval export "PIP_${token}=\$VAL_${token}"
+    fi
+    jq_inplace ".tokens.${token}.pip.addr = \"$(eval echo "\$PIP_${token}")\"" "$SETUP_CONFIG_FILE"
+done
 
-# Run Deployment
-"$BIN_DIR"/deploy-kovan.sh
+cp "$CONFIG_DIR/deploy-kovan.json" "$CONFIG_DIR/deploy-kovan-live.json"
+cp "$SETUP_CONFIG_FILE" "$CONFIG_DIR/deploy-kovan.json"
 
-echo "KOVAN DRYRUN PREP COMPLETED SUCCESSFULLY"
+echo "KOVAN LIKE PREP COMPLETED SUCCESSFULLY"
+echo "*****************************************************************"
+echo "*****                    STEPS TAKEN                        *****"
+echo "*****************************************************************"
+echo "***** - Sent ETH to OMNIA                                   *****"
+echo "***** - Deployed MEDIAN Pips for COLs                       *****"
+echo "***** - Saved PIP ADDR to prep config file                    *****"
+echo "***** - Copied real Kovan Config file to deploy-kovan-cp.json *****"
+echo "***** - Copied prep Config file to deploy-kovan.json          *****"
+echo "*****************************************************************"
+echo "*****           You can now run ./deploy-kovan.sh           *****"
+echo "*****                                                       *****"
+echo "*****                                                       *****"
+echo "*****             Afterwards you should run                 *****"
+echo "*****         ./scripts/setup/reset-kovan-like.sh           *****"
+echo "*****          to restore the kovan deploy config            *****"
+echo "*****************************************************************"
