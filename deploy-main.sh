2db53083c42a866b39f7add1d12232b74067207f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-06-26 13:09:24 -0300

    mainnet files + some changes in config files

---
+++ deploy-main.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# shellcheck source=lib/common.sh
+. "${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
+writeConfigFor "main"
+
+test "$(seth chain)" == "ethlive" || exit 1
+
+export DEPLOY_RESTRICTED_FAUCET="yes"
+"$LIBEXEC_DIR"/base-deploy
+
+log "MAINNET DEPLOYMENT COMPLETED SUCCESSFULLY"
