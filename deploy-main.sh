b751b4943d88016872bbd928fcc912dd53cd745c
Brian McMichael <brian@brianmcmichael.com>
2019-10-21 10:07:04 -0400

    Add etherscan verification config for mainnet deployment

--- deploy-main.sh
+++ deploy-main.sh
@@ -6,6 +6,13 @@ writeConfigFor "main"
 
 test "$(seth chain)" == "ethlive" || exit 1
 
+#Set verify contract option in Etherscan if the API key is in the config file
+etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
+if [[ "$etherscanApiKey" != "" ]]; then
+    export DAPP_VERIFY_CONTRACT="yes"
+    export ETHERSCAN_API_KEY=$etherscanApiKey
+fi
+
 export DEPLOY_RESTRICTED_FAUCET="yes"
 "$LIBEXEC_DIR"/base-deploy
 
