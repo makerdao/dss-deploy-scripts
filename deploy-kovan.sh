7c5c391d16a7ff5cf54e8f25fa4588a5fed3cbda
Brian McMichael <brian@brianmcmichael.com>
2019-10-21 10:06:36 -0400

    Re-enable dapp verify flags

--- deploy-kovan.sh
+++ deploy-kovan.sh
@@ -6,12 +6,12 @@ writeConfigFor "kovan"
 
 test "$(seth chain)" == "kovan" || exit 1
 
-# Set verify contract option in Etherscan if the API key is in the config file
-# etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
-# if [[ "$etherscanApiKey" != "" ]]; then
-#     export DAPP_VERIFY_CONTRACT="yes"
-#     export ETHERSCAN_API_KEY=$etherscanApiKey
-# fi
+#Set verify contract option in Etherscan if the API key is in the config file
+etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
+if [[ "$etherscanApiKey" != "" ]]; then
+    export DAPP_VERIFY_CONTRACT="yes"
+    export ETHERSCAN_API_KEY=$etherscanApiKey
+fi
 
 export DEPLOY_RESTRICTED_FAUCET="no"
 "$LIBEXEC_DIR"/base-deploy
