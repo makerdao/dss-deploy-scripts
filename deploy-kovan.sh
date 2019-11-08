33cc52b0f72d0292ee260358e915fad61c7d7d7a
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-11-08 17:47:25 -0300

    Remove etherscan verification old implementation

--- deploy-kovan.sh
+++ deploy-kovan.sh
@@ -6,13 +6,6 @@ writeConfigFor "kovan"
 
 test "$(seth chain)" == "kovan" || exit 1
 
-# Set verify contract option in Etherscan if the API key is in the config file
-# etherscanApiKey=$(jq -r ".etherscanApiKey" "$CONFIG_FILE")
-# if [[ "$etherscanApiKey" != "" ]]; then
-#     export DAPP_VERIFY_CONTRACT="yes"
-#     export ETHERSCAN_API_KEY=$etherscanApiKey
-# fi
-
 export DEPLOY_RESTRICTED_FAUCET="no"
 "$LIBEXEC_DIR"/base-deploy
 
