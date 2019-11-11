8e48fcead3114f990d48585379cae038bbea2055
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-11-11 14:52:05 -0300

    Add checks to control how user runs the commands

--- deploy-main.sh
+++ deploy-main.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ "$IN_NIX_SHELL" != "yes" ]]; then
+    echo "This command must be run via nix-shell"
+    exit 1
+fi
+if [[ -z "$BIN_DIR" ]]; then
+    echo "This command must be called without \"./\""
+    exit 1
+fi
+
 # shellcheck source=lib/common.sh
 . "${LIB_DIR:-$(cd "${0%/*}/lib"&&pwd)}/common.sh"
 writeConfigFor "main"
