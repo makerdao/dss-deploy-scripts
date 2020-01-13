993d8a42f361ebd58b66f33b131af031b354739b
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-01-13 11:32:45 -0300

    Minor fix for shellcheck + renaming nix package

--- shell.nix
+++ shell.nix
@@ -6,15 +6,15 @@
 }@args: with pkgs;
 
 let
-  tdds = import ./. args;
+  dds = import ./. args;
   dapp2nix = import (fetchGit {
     url = "https://github.com/icetan/dapp2nix";
     ref = "v2.0.1";
     rev = "0ecfc2f1086c8068a5abec8827997c8ee303e6d5";
   }) {};
 in mkShell {
-  buildInputs = tdds.bins ++ [
-    tdds
+  buildInputs = dds.bins ++ [
+    dds
     dapp2nix
     procps
   ];
@@ -23,7 +23,7 @@ in mkShell {
     export NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
 
     setup-env() {
-      . ${tdds}/lib/setup-env.sh
+      . ${dds}/lib/setup-env.sh
     }
     export -f setup-env
     setup-env || echo Re-run setup script with \'setup-env\'
