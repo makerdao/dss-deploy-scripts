dd08623113f2a77f6fb6b5994f7b84523efc1f89
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-01-29 17:27:08 -0300

    Upgrade nixpkgs-pin + new cachix endpoint

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,5 +1,5 @@
 import (fetchGit {
   url = "https://github.com/makerdao/nixpkgs-pin";
-  rev = "a5e96fb8b08d0f2d5b8b283d8f6081e9d87f4774";
+  rev = "c48563bd8bf79f3b23fa5b5d35c570e15d4d3ce2";
   ref = "master";
 })
