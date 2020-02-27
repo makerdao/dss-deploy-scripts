69d33c157969c3dd8e8c901797f8e289d7480ba9
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-02-27 09:15:31 -0300

    Add OsmMom deployment + upgrade to latest daptools version

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,5 +1,5 @@
 import (fetchGit {
   url = "https://github.com/makerdao/nixpkgs-pin";
-  rev = "c48563bd8bf79f3b23fa5b5d35c570e15d4d3ce2";
+  rev = "e9cc76b718cddc3d8fa642b880c8a45f5e7eb09e";
   ref = "master";
 })
