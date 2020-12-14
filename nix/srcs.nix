723b420ef78884e02179556ed5ec60e1a388c131
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-12-14 10:04:40 -0300

    Using solc 0.6.11 (not working yet)

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/makerdao/makerpkgs";
-    rev = "0cdc0e74ca1b4c2548d4eb36784ded03b416e8a6";
-    ref = "master";
+    rev = "2f63f1a03c2773b92368f1f042b53187724bdcdc";
+    ref = "fix-solc-versions";
   };
 }
